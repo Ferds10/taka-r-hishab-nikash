1d0f22922ea8ae8a2e44a5d85f2c01f4e85aea9b
ferds10 <mredhaforhad@gmail.com>
2022-02-18 13:53:59 +0900

    added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const incomeExpensBtn = document.getElementById('calculate-btn');
+const incomeExpensBtn = document.getElementById('income-expens-btn');
 const saveBtn = document.getElementById('save-btn');
 const totalExpensesField = document.getElementById('total-expense-field');
 const balanceField = document.getElementById('balance-field');
@@ -6,35 +6,30 @@ const savingsField = document.getElementById('savings-field');
 const remainingMoneyField = document.getElementById('Remaining-balance-field');
 
 // get value frome input box by dynamically.
-function getIncomeValue(boxId) {
-    let inputBox = document.getElementById(boxId);
-    let inputIncome = parseFloat(inputBox);
+function getIncomeValue(boxID) {
+    let inputBox = document.getElementById(boxID);
+    let inputIncome = parseFloat(inputBox.value);
     if (isNaN(inputIncome) || inputIncome < 0) {
         inputBox.value = '';
         return alert('Please input valid amount of money in number and positive format');
-
     };
     inputBox.value = '';
     return inputIncome;
-
 }
 
 //update Balance
 incomeExpensBtn.addEventListener('click', function (event) {
     event.preventDefault();
     let balanceAmount = parseFloat(balanceField.innerText);
-    let inputIncome = getIncomeValue('income-box');
-    console.log(inputIncome);
-
-
-
-
-    // balanceField.innerText = inputIncome + balance;
-    // remainingMoneyField.innerText = inputIncome + remainingBalance;
-    // incomeBox.value = '';
-
-
-});
+    let inputAmount = getIncomeValue('income-box');
+    // console.log(inputAmount);
+})
+// incomeExpensBtn.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     let balanceAmount = parseFloat(balanceField.innerText);
+//     let inputAmount = getIncomeValue('income-box');
+//     console.log(inputAmount);
+// });
 
 // incomeExpensBtn.addEventListener('click', function (event) {
 //     event.preventDefault();
@@ -48,9 +43,9 @@ incomeExpensBtn.addEventListener('click', function (event) {
 //     return alert('Please input valid amount of money in number and positive format');
 // };
 
+ // balanceField.innerText = inputIncome + balance;
+    // remainingMoneyField.innerText = inputIncome + remainingBalance;
+    // incomeBox.value = '';
+// });
 
 
-saveBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-
-});
